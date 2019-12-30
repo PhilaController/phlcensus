68ae6b9de8c60586136c7206484b7ce1db99e0d4
Nick Hand <nicholas.adam.hand@gmail.com>
2019-12-30 13:00:53 -0500

    update ACS tests

--- phlcensus/acs/tests/test_acs.py
+++ phlcensus/acs/tests/test_acs.py
@@ -1,4 +1,5 @@
 from phlcensus.acs import DATASETS
+from phlcensus.regions import *
 import pytest
 
 
@@ -6,6 +7,8 @@ import pytest
 def test_tract_level(cls):
 
     data = cls.get(level="tract")
+    tracts = CensusTracts.get()
+    assert len(data) == len(tracts)
 
 
 @pytest.mark.parametrize("cls", DATASETS.values())
@@ -17,5 +20,31 @@ def test_city_level(cls):
 
 @pytest.mark.parametrize("cls", DATASETS.values())
 def test_puma_level(cls):
-
     data = cls.get(level="puma")
+    pumas = PUMAs.get()
+    assert len(data) == len(pumas)
+
+
+@pytest.mark.parametrize("cls", DATASETS.values())
+def test_count_aggregation(cls):
+
+    if cls.AGGREGATION == "count":
+
+        # data by census tract
+        data_by_tract = cls.get(level="tract")
+
+        # columns to test
+        cols = list(cls.RAW_FIELDS.values())
+
+        # NTAs
+        hoods = NTAs.get()
+
+        # Test other levels
+        for level in ["nta"]:
+            data = cls.get(level=level)
+
+            # check the number of hoods
+            assert len(data) == len(hoods)
+
+            # check the total sum
+            assert (data_by_tract[cols].sum() == data[cols].sum()).all()

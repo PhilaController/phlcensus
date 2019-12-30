9571e958e8abab9df218e293da9c9e58bbe814f6
Nick Hand <nicholas.adam.hand@gmail.com>
2019-12-30 13:10:37 -0500

    bug fix for snap

--- phlcensus/acs/snap.py
+++ phlcensus/acs/snap.py
@@ -4,13 +4,14 @@ import collections
 __all__ = ["SNAP"]
 
 
-class SNAP(CensusDataset):
+class SNAP(ACSDataset):
     """
     Household received Food Stamps/SNAP in the past 12 months
     """
 
     UNIVERSE = "household"
     TABLE_NAME = "B22003"
+    AGGREGATION = "count"
     RAW_FIELDS = collections.OrderedDict(
         {"001": "universe", "002": "recipient", "05": "nonrecipient"}
     )
